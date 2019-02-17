4f0ab37aba8ec9c683d68235d29832ce431be36a
Christos Georgiou <georgiou.christos@outlook.com>
2019-02-17 17:02:06 +0200

    add missing vendors

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,14 @@
 const gulp = require('gulp')
-require('gulp-cg-toolkit')(gulp)
+require('gulp-cg-toolkit')(gulp, {
+
+    files: [{
+        source: './src/static/**/*.*',
+        destination: './dist/'
+    }, {
+        source: './node_modules/jquery/dist/**/*.*',
+        destination: './dist/vendor/jquery'
+    }, {
+        source: './node_modules/bootstrap/dist/**/*.*',
+        destination: './dist/vendor/bootstrap/'
+    }]
+})
